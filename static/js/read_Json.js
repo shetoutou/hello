5022ca1b37d470bb59ded404b7ee1da3d25542e4
shetoutou <40586429+shetoutou@users.noreply.github.com>
2024-08-01 16:15:32 +0800

    Update read_Json.js
    增加icon判断功能，若icon加载不正常，启动本地icon图片顶替
    
    Signed-off-by: shetoutou <40586429+shetoutou@users.noreply.github.com>

--- static/js/read_Json.js
+++ static/js/read_Json.js
@@ -9,14 +9,55 @@ function CreateLink(file) {
 	void(head.appendChild(new_element));
 };
 
-//加载失败随机获取内置icon
-function geticon(th) {
-	//获取（1,3)随机数
-	var num = Math.floor(Math.random()*(3-1+1))+1;
+//随机获取本地iocn图片，返回地址
+function getFloorIcon() {
+	var minNum = 1;
+	var maxNum = 8;
+	//获取（1,8)随机数
+	var num = Math.floor(Math.random()*(maxNum-minNum+1))+minNum;
 	var url = "static/img/noico_"+num+".png"
-	th.src = url;
+	return url;
+};
+
+// 数字四舍五入, value: 数据源,n: 保留几位小数
+function retain(value, n) {
+	if(n==='null' || n === 'undefined' || n === 0) return parseInt(value);
+	let tran = Math.round(value * Math.pow(10, n)) / Math.pow(10, n);
+	let tranV = tran.toString();
+	let newVal = tranV.indexOf('.');
+	if(newVal < 0) {
+		tranV += '.'
+	};
+	for(let i = tranV.length - tranV.indexOf('.'); i <= n; i++) {
+		tranV += '0';
+	};
+	return tranV
+}
+//icon二进制流判断字节大小并随机抽取本地icon顶替
+function preloadImage(url,th) {
+	var ByteKb ;
+	fetch(url).then(function(res){
+		return res.blob();
+	}).then(function(data){
+		ByteKb = retain(data.size/1024,4);
+		//console.log("icon大小KB:"+ByteKb);
+		if (ByteKb === 0.6123){
+			th.src = getFloorIcon();
+			return ;
+		}
+		return ;	
+	});
+}
+//icon加载失败后执行顶替
+function geticon(th) {
+	th.src = getFloorIcon();
 	//console.log(th+"错误的icon执行了备用icon"+url);
 };
+//icon加载成功后判断片大小是否真实
+function ifIcon(th) {
+	preloadImage(th.src,th);
+};
+
 
 $(document).ready(function() {
 	$.getJSON("static/json/data.json",function(data){
@@ -30,8 +71,9 @@ $(document).ready(function() {
 				if (attr == info["name"]){
 					var str = info["data"];
 					for (var j=0;j<str.length;j++){
+						var iconurl = 'https://www.favicon.vip/get.php?url='+str[j]["url"];
 						strHtml = strHtml + '<li class="col-3 col-sm-3 col-md-3 col-lg-1"><a rel="nofollow" href="'+ str[j]["url"]+'" target="_blank">'
-						strHtml = strHtml + '<img class="icon" src="https://www.favicon.vip/get.php?url='+str[j]["url"]+'" onerror="geticon(this)" />';
+						strHtml = strHtml + '<img class="icon" src="'+iconurl+'" onerror="geticon(this)"  onload="ifIcon(this)" />';
 						strHtml = strHtml + '<span>'+str[j]["name"]+'</span></a></li>';
 					};
 					ulTag.eq(i).children('li:not(:first)' ).remove();
